e9beff7b8f994b7e8f3ba4499a5021c4b0d13182
pompopo <pompopo@gmail.com>
2012-12-24 04:19:41 +0900

    少しだけコメントを追加

--- PMResult.h
+++ PMResult.h
@@ -10,7 +10,12 @@
 @class PMWord;
 
 @interface PMResult : NSObject
+// 形態素の総数を返します。
 @property (nonatomic) int totalCount;
+
+// フィルタにマッチした形態素数を返します。
 @property (nonatomic) int filteredCount;
+
+// 形態素のリストを返します。
 @property (nonatomic, retain) NSArray *wordList;
 @end
--- PMWord.h
+++ PMWord.h
@@ -9,9 +9,18 @@
 #import <Foundation/Foundation.h>
 
 @interface PMWord : NSObject
+// 形態素の表記を返します。
 @property (nonatomic, copy) NSString *surface;
+
+// 形態素の読みがなを返します。
 @property (nonatomic, copy) NSString *reading;
+
+// 形態素の品詞を返します。
 @property (nonatomic, copy) NSString *pos;
+
+// 形態素の基本形表記を返します。活用のない形態素の場合は省略されます。
 @property (nonatomic, copy) NSString *baseform;
+
+// uniq_result 中の word 内に現れる、形態素の出現数を返します。
 @property (nonatomic) int count;
 @end
